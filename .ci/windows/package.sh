cec2863b68fdbc49fe5b540a817d5166681ee523
Luke Ceddia <flukiluke@gmail.com>
2023-01-03 23:33:00 +1100

    Fix packaging

--- .ci/windows/package.sh
+++ .ci/windows/package.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -x
 
 UNUSED_ARCHES="aarch64-w64-mingw32 armv7-w64-mingw32 i686-w64-mingw32"
 
@@ -8,8 +9,8 @@ for arch in $UNUSED_ARCHES; do
     rm -r "llvm-mingw/${arch}"
 done
 
-rm -r include python lib/{libear,libscanbuild}
+rm -r llvm-mingw/{include,python,lib/libear,lib/libscanbuild}
 mv llvm-mingw out/llvm
 mv out "lbasic-${version}"
 mkdir release
-7z a "release/lbasic-${version}-windows-x86_64.7z"
+7z a "release/lbasic-${version}-windows-x86_64.7z" "lbasic-${version}"
