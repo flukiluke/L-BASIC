64b0222525c9447c06f1317a627b3ef1e304eb1d
Luke Ceddia <flukiluke@gmail.com>
2023-01-03 19:43:10 +1100

    Fix file hash

--- .ci/windows/dependencies.sh
+++ .ci/windows/dependencies.sh
@@ -2,12 +2,12 @@
 set -ex
 
 QB64_URL='https://github.com/QB64-Phoenix-Edition/QB64pe/releases/download/v3.4.1/qb64pe_win-x64-3.4.1.7z'
-QB64_SHA256=a47213e0a2e6d01e2fdf0db53e20dfb86131511230cf242ea7b372c90aa8d553
+QB64_SHA256=41ba1d4da734c2c2ac309d923b48a9ccf3afbf936b1d23a3be9a885840a7f95d
 LLVM_MINGW_URL='https://github.com/mstorsjo/llvm-mingw/releases/download/20220323/llvm-mingw-20220323-ucrt-x86_64.zip'
 LLVM_MINGW_SHA256=3014a95e4ec4d5c9d31f52fbd6ff43174a0d9c422c663de7f7be8c2fcc9d837a
 
-wget "${QB64_URL}" -O qb64.7z
-wget "${LLVM_MINGW_URL}" -O llvm_mingw.zip
+wget --no-verbose "${QB64_URL}" -O qb64.7z
+wget --no-verbose "${LLVM_MINGW_URL}" -O llvm_mingw.zip
 
 sha256sum --check << EOT
 ${QB64_SHA256} qb64.7z
