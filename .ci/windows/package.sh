99dc58baa66963c74151b3a8cc35b4e1f20ca30e
Luke Ceddia <flukiluke@gmail.com>
2023-01-03 23:22:03 +1100

    Release build

---
+++ .ci/windows/package.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+UNUSED_ARCHES="aarch64-w64-mingw32 armv7-w64-mingw32 i686-w64-mingw32"
+
+version=$(git describe)
+
+for arch in $UNUSED_ARCHES; do
+    rm -r "llvm-mingw/${arch}"
+done
+
+rm -r include python lib/{libear,libscanbuild}
+mv llvm-mingw out/llvm
+mv out "lbasic-${version}"
+mkdir release
+7z a "release/lbasic-${version}-windows-x86_64.7z"
