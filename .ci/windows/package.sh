46cf057080d59a1453cc7e739da02ac594cf11dd
Luke Ceddia <flukiluke@gmail.com>
2023-01-04 16:49:05 +1100

    Remove unused files

--- .ci/windows/package.sh
+++ .ci/windows/package.sh
@@ -4,13 +4,12 @@ set -x
 UNUSED_ARCHES="aarch64-w64-mingw32 armv7-w64-mingw32 i686-w64-mingw32"
 
 for arch in $UNUSED_ARCHES; do
-    rm -r "llvm-mingw/${arch}"
+    rm -r "out/llvm/${arch}"
 done
+rm -r out/llvm/{include,python,lib/libear,lib/libscanbuild} out/{lbasic.bas,llvm.h}
 
 version=$GITHUB_REF_NAME
 
-rm -r llvm-mingw/{include,python,lib/libear,lib/libscanbuild}
-rm out/{lbasic.bas,llvm.h}
 mv out "lbasic-${version}"
 mkdir release
 7z a "release/lbasic-${version}-windows-x86_64.7z" "lbasic-${version}"
